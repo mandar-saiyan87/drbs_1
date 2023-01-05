ce984c4e68af1533d6833f0d7f0ab5d7f805c402
mandar-saiyan87 <mdman2257@gmail.com>
2023-01-05 15:48:20 +0530

    firebase update

--- src/components/context/Firebase/firebaseConfig.js
+++ src/components/context/Firebase/firebaseConfig.js
@@ -9,12 +9,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDkFqQJ0n3B6s2Xl8S7WMiAY66WLvSRBIA",
-  authDomain: "drbs-f24ae.firebaseapp.com",
-  projectId: "drbs-f24ae",
-  storageBucket: "drbs-f24ae.appspot.com",
-  messagingSenderId: "309930952665",
-  appId: "1:309930952665:web:dd600bbcb17822fb7fbbc8"
+  apiKey: "AIzaSyDUpI092ArBNHrguueF6hQPrhqxsNUueXE",
+  authDomain: "drbs-4d97a.firebaseapp.com",
+  projectId: "drbs-4d97a",
+  storageBucket: "drbs-4d97a.appspot.com",
+  messagingSenderId: "337612622586",
+  appId: "1:337612622586:web:39c8412fd72bd92b76d743"
 };
 
 // Initialize Firebase
