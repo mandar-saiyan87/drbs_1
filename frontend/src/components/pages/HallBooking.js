d2d336298d35cd8b53b8b8e77ad35c2d8cd6abb1
mandar-saiyan87 <mdman2257@gmail.com>
2023-12-14 12:08:01 +0530

    new upload

---
+++ frontend/src/components/pages/HallBooking.js
@@ -0,0 +1,84 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { AppContext } from '../context/AppContext';
+import { useNavigate } from "react-router-dom";
+import exportFromJSON from 'export-from-json';
+
+
+export default function HallBooking() {
+
+  const context = useContext(AppContext);
+
+  const { bookings, getBookings, deleteBooking, searchBooking } = context;
+  const navigate = useNavigate();
+
+  const [bookingSearch, setBookingSearch] = useState("")
+
+  const data = bookings;
+  const filename = 'download';
+  const exportType = exportFromJSON.types.csv;
+
+  function ExporttoExcel() {
+    exportFromJSON({ data, filename, exportType })
+  }
+
+
+  useEffect(() => {
+    if (bookingSearch.length === 0) {
+      getBookings();
+    }
+    // eslint-disable-next-line
+  }, [deleteBooking, bookingSearch]);
+
+  function updateBookingDetails(bookingData) {
+    navigate('/editHallBooking', { state: bookingData })
+  }
+
+  function handleSearch() {
+    searchBooking(bookingSearch)
+  }
+
+  return (
+    <>
+      <div className='px-9'>
+        <Link to="/hallbookingform">
+          <div className='bg-blue-500 p-3 my-5 text-white hover:bg-gray-400 cursor-pointer max-w-max'>New Booking</div>
+        </Link>
+        <div className='bg-blue-500 p-3 my-5 text-white hover:bg-gray-400 cursor-pointer max-w-max' onClick={ExporttoExcel}>Export</div>
+        <div className='flex flex-row'>
+          <input type="text" value={bookingSearch} className='w-[20%]' onChange={(e) => setBookingSearch(e.target.value)} placeholder='Search' />
+          <div className='bg-blue-500 px-4 text-white hover:bg-gray-400 cursor-pointer' onClick={handleSearch}>SEARCH</div>
+        </div>
+        <div className='grid grid-cols-5 mt-10 w-[85%]'>
+          <div className='flex text-left'>संपूर्ण नांव</div>
+          <div className='flex text-left'>सभागृह</div>
+          <div className='flex text-left'>कार्यक्रम</div>
+          <div className='flex text-left'>दिवस</div>
+          <div className='flex text-left'>बुकिंगचा प्रकार</div>
+        </div>
+        <hr />
+        <div>
+          <div className='my-5'></div>
+          {bookings.map((booking, index) => {
+            return (
+              <div className='flex flex-row my-5'>
+                <div className='grid grid-cols-5 w-[85%]' key={index}>
+                  <div className='flex items-center justify-start'><p>{booking.fullname}</p></div>
+                  <div className='flex items-center justify-start'><p>{booking.hallno}</p></div>
+                  <div className='flex items-center justify-start'><p>{booking.event}</p></div>
+                  <div className='flex items-center justify-start'><p>{booking.eventdate}</p></div>
+                  <div className='flex items-center justify-start'><p>{booking.bookingtype}</p></div>
+                </div>
+                <div className='flex flex-row items-center justify-center'>
+                  <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => updateBookingDetails(booking)}>EDIT</div>
+                  <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => deleteBooking(booking.id)}>DELETE</div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
---
+++ frontend/src/components/pages/MembersPage.js
@@ -0,0 +1,111 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { AppContext } from '../context/AppContext';
+import { useNavigate } from "react-router-dom";
+import exportFromJSON from 'export-from-json';
+
+function MembersPage() {
+
+  const context = useContext(AppContext);
+
+  const { members, getMembers, deleteMember, searchMember } = context;
+  const navigate = useNavigate();
+
+  const [search, setSearch] = useState("")
+
+  // const sortedMembers = [...members].sort((a, b) => a.memberno - b.memberno)
+
+  const data = members;
+  const filename = 'download';
+  const exportType = exportFromJSON.types.csv;
+
+  function ExporttoExcel() {
+    exportFromJSON({ data, filename, exportType })
+  }
+
+  useEffect(() => {
+    if (search.length === 0) {
+      getMembers();
+    }
+    // eslint-disable-next-line
+  }, [deleteMember, search, members]);
+
+  function updateMemberDetails(memberData) {
+    navigate('/editMembers', { state: memberData })
+  }
+
+
+  function handleSearch() {
+    searchMember(search)
+  }
+
+  return (
+    <>
+      <div className='px-9'>
+        <Link to="/membersform">
+          <div className='bg-blue-500 p-3 my-5 text-white hover:bg-gray-400 cursor-pointer max-w-max'>Add Member</div>
+        </Link>
+        <div className='bg-blue-500 p-3 my-5 text-white hover:bg-gray-400 cursor-pointer max-w-max' onClick={ExporttoExcel}>Export</div>
+        <div className='flex flex-row'>
+          <input type="text" value={search} className='w-[20%]' onChange={(e) => setSearch(e.target.value)} placeholder='Search' />
+          <div className='bg-blue-500 px-4 text-white hover:bg-gray-400 cursor-pointer' onClick={handleSearch}>SEARCH</div>
+        </div>
+        <div className='grid grid-cols-5 mt-10 w-[85%]'>
+          <div className='flex text-left'>सभासद क्र.</div>
+          <div className='flex text-left'>संपूर्ण नांव</div>
+          <div className='flex text-left'>भ्रमणध्वनी</div>
+          <div className='flex text-left'>सभासद वर्ग</div>
+          <div className='flex text-left'>लाईफ स्टेटस</div>
+        </div>
+        <hr />
+        <div>
+          <div className='my-5'></div>
+          {members.sort((a, b) => a.memberno - b.memberno).map((member) => {
+            return (
+              <div className='flex flex-row my-5'>
+                <div className='grid grid-cols-5 w-[85%]' key={member}>
+                  <div className='flex items-center justify-start'>
+                    <p>{member.memberno}</p>
+                  </div>
+                  <div className='flex items-center justify-start'><p>{member.fullname}</p></div>
+                  <div className='flex items-center justify-start'><p>{member.mobile === "" ? '-' : member.mobile}</p></div>
+                  <div className='flex items-center justify-start'><p>{member.membership}</p></div>
+                  <div className='flex items-center justify-start'><p>{member.livingStatus}</p></div>
+                </div>
+                <div className='flex flex-row items-center justify-center'>
+                  <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => updateMemberDetails(member)}>EDIT</div>
+                  <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => deleteMember(member.id)}>DELETE</div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MembersPage
+
+
+// {
+//   members.sort((a, b) => a.memberno > b.memberno ? b : a).map((member) => {
+//     return (
+//       <div className='flex flex-row my-5'>
+//         <div className='grid grid-cols-5 w-[85%]' key={member}>
+//           <div className='flex items-center justify-start'>
+//             <p>{member.memberno}</p>
+//           </div>
+//           <div className='flex items-center justify-start'><p>{member.fullname}</p></div>
+//           <div className='flex items-center justify-start'><p>{member.mobile === "" ? '-' : member.mobile}</p></div>
+//           <div className='flex items-center justify-start'><p>{member.membership}</p></div>
+//           <div className='flex items-center justify-start'><p>{member.livingStatus}</p></div>
+//         </div>
+//         <div className='flex flex-row items-center justify-center'>
+//           <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => updateMemberDetails(member)}>EDIT</div>
+//           <div className='bg-blue-500 px-4 mx-2 text-white hover:bg-gray-400 cursor-pointer' onClick={() => deleteMember(member.id)}>DELETE</div>
+//         </div>
+//       </div>
+//     )
+//   })
+// }
