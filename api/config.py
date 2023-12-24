05cb8b50192409c42c4eba265f57a5cfaec4b6de
mandar-saiyan87 <mdman2257@gmail.com>
2023-12-24 23:05:19 +0530

    db updated 2

--- api/config.py
+++ api/config.py
@@ -5,7 +5,7 @@ db_passwd = os.getenv('MONGO_PASS')
 
 
 class DBConfig:
-    MONGO_URI = f'mongodb+srv://mandarcofiato87:{db_passwd}@cluster0.pontizx.mongodb.net/drbsDB?retryWrites=true&w=majority'
+    MONGO_URI = f'mongodb+srv://mandarcofiato87:{db_passwd}@cluster0.pontizx.mongodb.net/?retryWrites=true&w=majority'
 
 
 class AppConfig:
